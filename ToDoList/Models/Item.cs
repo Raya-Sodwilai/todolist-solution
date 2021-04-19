2ae7cd670bb3f5ff1edeb547556ad95acde8bdca
Raya Sodwilai <sodwilai@pdx.edu>
2021-04-19 14:45:49 -0700

    update logic

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; }
+    // We add set; to the line below.
+    public string Description { get; set; }
+    private static List<Item> _instance = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
+      _instance.Add(this);
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instance;
+    }
+
+    public static void ClearAll()
+    {
+      _instance.Clear();
     }
   }
 }
