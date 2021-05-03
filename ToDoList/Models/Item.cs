fbbf56c9cbae64e57d4beac32dadc1581717e9e9
Raya Sodwilai <sodwilai@pdx.edu>
2021-05-03 14:27:15 -0700

    comment out broken tests

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -18,6 +18,20 @@ namespace ToDoList.Models
       Id = id;
     }
 
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+        Item newItem = (Item) otherItem;
+        bool descriptionEquality = (this.Description == newItem.Description);
+        return descriptionEquality;
+      }
+    }
+
     public static List<Item> GetAll()
     {
       List<Item> allItems = new List<Item> { };
@@ -45,13 +59,13 @@ namespace ToDoList.Models
     {
       MySqlConnection conn = DB.Connection();
       conn.Open();
-      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      var cmd = conn.CreateCommand() as MySqlCommand;
       cmd.CommandText = @"DELETE FROM items;";
       cmd.ExecuteNonQuery();
       conn.Close();
       if (conn != null)
       {
-        conn.Dispose();
+       conn.Dispose();
       }
     }
     public static Item Find(int searchId)
